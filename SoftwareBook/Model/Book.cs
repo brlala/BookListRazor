b68432c751e9283f32b5638cd2a81f502e173caf
liheng <liheng@pand.ai>
2020-04-09 00:32:59 +0800

    setting up database

--- SoftwareBook/Model/Book.cs
+++ SoftwareBook/Model/Book.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SoftwareBook.Model
 {
-    public class Class
+    public class Book
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public string Author { get; set; }
     }
 }
