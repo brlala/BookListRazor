64dcfd5850ee77767a3fd8ba4e56a128bef2d41d
liheng <liheng@pand.ai>
2020-04-09 11:26:57 +0800

    compeleted CRUD

--- SoftwareBook/Pages/BookList/Edit.cshtml.cs
+++ SoftwareBook/Pages/BookList/Edit.cshtml.cs
@@ -4,14 +4,38 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SoftwareBook.Model;
 
 namespace SoftwareBook.Pages.BookList
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _db;
+        public EditModel(ApplicationDbContext db)
         {
+            _db = db;
+        }
+        [BindProperty]
+        public Book Book { get; set; }
+        public async Task OnGet(int id)
+        {
+            Book = await _db.Book.FindAsync(id);
+        }
 
+        public async Task<IActionResult> OnPost()
+        {
+            if(ModelState.IsValid)
+            {
+                var BookFromDb = await _db.Book.FindAsync(Book.Id);
+                BookFromDb.Name = Book.Name;
+                BookFromDb.ISBN = Book.ISBN;
+                BookFromDb.Author = Book.Author;
+
+                await _db.SaveChangesAsync();
+
+                return RedirectToPage("Index");
+            }
+            return RedirectToPage("Index");
         }
     }
 }
