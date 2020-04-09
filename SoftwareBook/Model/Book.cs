411b696cc8a2a99ef43f82e7551dd3b1fcb64bdb
liheng <liheng@pand.ai>
2020-04-09 10:53:48 +0800

    added create page

--- SoftwareBook/Model/Book.cs
+++ SoftwareBook/Model/Book.cs
@@ -14,5 +14,6 @@ namespace SoftwareBook.Model
         [Required]
         public string Name { get; set; }
         public string Author { get; set; }
+        public string ISBN { get; set; }
     }
 }
